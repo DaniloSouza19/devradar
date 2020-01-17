917d6ea57c6e23f240a6d29d40ee2264c3b25fd9
Danilo Souza <danilo_souza19@outlook.com>
2020-01-17 09:02:58 -0300

    backend | .gitignore added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const {MONGO_USER, MONGO_DB, MONGO_PASS} = require('../.env');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://danilo:dgss3nha@project01-zzohc.mongodb.net/week10?retryWrites=true&w=majority',{
+mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@project01-zzohc.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
